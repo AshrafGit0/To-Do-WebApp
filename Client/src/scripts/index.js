6722b390981297cac001f26de8df00b8510f655d
Ashraf <mohammedashrafali377@gmail.com>
2024-11-10 09:03:27 +0530

    initial commit

--- Client/src/scripts/index.js
+++ Client/src/scripts/index.js
@@ -2,22 +2,20 @@ var taskNum;
 var TaskId;
 function LoadPage(pageName) {
     $.ajax({
+        url: `../../../Client/public/${pageName}`,
         method: "get",
-        url: `../../public/${pageName}`,
         success: (response) => {
             $("section").show();
             $("section").html(response);
             $("#BtnContainer").hide();
         }
     })
+
 }
-function ResetId()
-{
-    
-}
-function LoadMain(){
+
+function LoadMain() {
     $("#main").html("");
-    taskNum=0;
+    taskNum = 0;
     $.ajax({
         method: "get",
         url: `http://127.0.0.1:6600/get-tasks/${$.cookie('Userid')}`,
@@ -27,7 +25,7 @@ function LoadMain(){
                     `
 
                     <div class="alert alert-success ">
-                        <h4>${taskNum+=1}.${task.Title}</h4>
+                        <h4>${taskNum += 1}.${task.Title}</h4>
                         <div>
                             <p>${task.Description}</p>
                         </div>
@@ -42,7 +40,7 @@ function LoadMain(){
                          </div>
                          
                          `
-                        ).appendTo("#main");
+                ).appendTo("#main");
             })
         }
     })
@@ -51,20 +49,19 @@ function LoadMain(){
     $("#Username").html($.cookie("UserName"));
 }
 $(() => {
-    if($.cookie("Userid"))
-        {
-            LoadPage(`dashboard.html`);
-            LoadMain();
-        }else{
-            LoadPage('signin.html')
-        }
+    if ($.cookie("Userid")) {
+        LoadPage(`dashboard.html`);
+        LoadMain();
+    } else {
+        LoadPage('signin.html')
+    }
     $.ajax({
-        method:"get",
-        url:"http://127.0.0.1:6600/get-tasks",
-        success:(tasks)=>{
-           TaskId = tasks.length + 1 ;
-           $("#Task-Id").val(TaskId);
-           
+        method: "get",
+        url: "http://127.0.0.1:6600/get-tasks",
+        success: (tasks) => {
+            TaskId = tasks.length + 1;
+            $("#Task-Id").val(TaskId);
+
         }
     });
     $(document).on("click", "#BtnRegister", () => {
@@ -77,7 +74,7 @@ $(() => {
         $("#BtnContainer").show();
         $("section").hide();
     })
-    $(document).on("click","#BtnSignout",()=>{
+    $(document).on("click", "#BtnSignout", () => {
         $.removeCookie("Userid");
         $.removeCookie("UserPwd");
         location.href = "./index.html";
@@ -89,12 +86,12 @@ $(() => {
             success: (users) => {
                 var user = users.find(user => user.UserId === $("#UserId").val());
                 if (user.Password === $("#Password").val()) {
-                    $.cookie("Userid",user.UserId,{expire:365});
-                    $.cookie("UserName",user.UserName,{expire:365});
-                    if($.cookie("Userid"))
-                    LoadPage(`dashboard.html`);
+                    $.cookie("Userid", user.UserId, { expire: 365 });
+                    $.cookie("UserName", user.UserName, { expire: 365 });
+                    if ($.cookie("Userid"))
+                        LoadPage(`dashboard.html`);
                     LoadMain();
-                    
+
 
                 } else {
                     alert('Invalid Password')
@@ -102,85 +99,85 @@ $(() => {
             }
         })
     })
-    $(document).on("click","#btn-Add-Task",()=>{
+    $(document).on("click", "#btn-Add-Task", () => {
         var task = {
-            AppointmentId : $("#Task-Id").val(),
-            Title : $("#Task-Title").val(),
-            Description : $("#Task-Description").val(),
-            Date : ($("#Task-Date").val()),
-            UserId : $.cookie('Userid')
+            AppointmentId: $("#Task-Id").val(),
+            Title: $("#Task-Title").val(),
+            Description: $("#Task-Description").val(),
+            Date: ($("#Task-Date").val()),
+            UserId: $.cookie('Userid')
         }
         $.ajax({
-            method:"post",
-            url:"http://127.0.0.1:6600/add-task",
-            data:task,
-            success: ()=>{
+            method: "post",
+            url: "http://127.0.0.1:6600/add-task",
+            data: task,
+            success: () => {
                 console.log('Task Added Successfully');
                 LoadMain();
             }
 
         })
     })
-    $(document).on("click","#BtnFormRegister",()=>{
+    $(document).on("click", "#BtnFormRegister", () => {
         var RegisterUser = {
-            UserId : $("#rgstUserId").val(),
-            UserName : $("#rgstUserName").val(),
-            Password : $("#rgstPassword").val(),
-            Email : $("#rgstEmail").val(),
-            Mobile : $("#rgstMobile").val()
+            UserId: $("#rgstUserId").val(),
+            UserName: $("#rgstUserName").val(),
+            Password: $("#rgstPassword").val(),
+            Email: $("#rgstEmail").val(),
+            Mobile: $("#rgstMobile").val()
         }
         $.ajax({
-            method:"post",
-            url:"http://127.0.0.1:6600/register-user",
-            data : RegisterUser,
-            success: ()=>{
+            method: "post",
+            url: "http://127.0.0.1:6600/register-user",
+            data: RegisterUser,
+            success: () => {
                 LoadPage('signin.html');
                 console.log('User Registered Successfully');
             }
         })
     })
-    $(document).on("click","#btn-EditTask",(e)=>{
+    $(document).on("click", "#btn-EditTask", (e) => {
         $.ajax({
-            method:"get",
-            url:`http://127.0.0.1:6600/get-appointments/${e.target.value}`,
-            success: (task)=>{
+            method: "get",
+            url: `http://127.0.0.1:6600/get-appointments/${e.target.value}`,
+            success: (task) => {
                 var editDatestr = task.Date;
                 $("#Edit-TaskId").val(task.AppointmentId);
                 $("#Edit-TaskTitle").val(task.Title);
                 $("#Edit-TaskDescription").val(task.Description);
-                $("#Edit-TaskDate").val(editDatestr.substring(editDatestr.indexOf("T"),0));
+                $("#Edit-TaskDate").val(editDatestr.substring(editDatestr.indexOf("T"), 0));
                 LoadMain();
             },
-            error:(err)=>{
+            error: (err) => {
                 console.log(err);
             }
         })
     })
     // Deleting Tasks
 
-    $(document).on("click","#btn-DeleteTask",(e)=>{
+    $(document).on("click", "#btn-DeleteTask", (e) => {
         $.ajax({
-            method:"delete",
-            url:`http://127.0.0.1:6600/delete-task/${e.target.value}`,
-            success: ()=>{
+            method: "delete",
+            url: `http://127.0.0.1:6600/delete-task/${e.target.value}`,
+            success: () => {
                 LoadMain();
             }
         })
     })
     // Updating Existing task
-    $(document).on("click","#btn-Update-Task",()=>{
+    $(document).on("click", "#btn-Update-Task", () => {
         var updateTask = {
-            AppointmentId : $("#Edit-TaskId").val(),
-            Title : $("#Edit-TaskTitle").val(),
-            Description : $("#Edit-TaskDescription").val(),
-            Date : ($("#Edit-TaskDate").val()),
-            UserId : $.cookie('Userid')
+            AppointmentId: $("#Edit-TaskId").val(),
+            Title: $("#Edit-TaskTitle").val(),
+            Description: $("#Edit-TaskDescription").val(),
+            Date: ($("#Edit-TaskDate").val()),
+            UserId: $.cookie('Userid')
         }
         $.ajax({
-            method:"put",
-            url:`http://127.0.0.1:6600/update-task/${$("#Edit-TaskId").val()}`,
+            method: "put",
+            url: `http://127.0.0.1:6600/update-task/${$("#Edit-TaskId").val()}`,
             data: updateTask,
-            success: ()=>{
+            success: () => {
                 LoadMain();
             }
         })
